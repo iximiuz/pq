7b059d927087fde6608a31ce9b3d08236ceb2a05
Ivan Velichko <iximiuz@gmail.com>
2021-05-09 21:18:18 +0200

    teach the engine to match vector selector

--- src/engine/engine.rs
+++ src/engine/engine.rs
@@ -1,5 +1,4 @@
 use crate::input::Input;
-use crate::labels::MatchOp;
 use crate::parser::ast::*;
 
 pub struct Engine {}
@@ -14,24 +13,16 @@ impl Engine {
             NodeKind::VectorSelector(ref selector) => loop {
                 let record = match input.take_one().unwrap() {
                     Some(r) => r,
-                    None => break,
+                    None => return,
                 };
 
-                let mut matched = true;
-                for matcher in selector.matchers().iter() {
-                    matched = match record.label(matcher.label()) {
-                        Some(label) => match matcher.match_op() {
-                            MatchOp::Eql => label == matcher.value(),
-                            MatchOp::Neq => label != matcher.value(),
-                            _ => unimplemented!(),
-                        },
+                let matched = selector
+                    .matchers()
+                    .iter()
+                    .all(|m| match record.label(m.label()) {
+                        Some(v) => m.matches(v),
                         None => false,
-                    };
-
-                    if !matched {
-                        break;
-                    }
-                }
+                    });
 
                 if matched {
                     println!("{:?}", record);
