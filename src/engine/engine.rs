1ca62c1e5592aa4d61178c114fc405817ffab927
Ivan Velichko <iximiuz@gmail.com>
2021-05-12 21:57:49 +0200

    Add use cases with corresponding queries

--- src/engine/engine.rs
+++ src/engine/engine.rs
@@ -3,6 +3,31 @@ use crate::parser::ast::*;
 
 pub struct Engine {}
 
+// - Requests longer than 500ms
+// duration > 500ms
+//
+// - Requests longer than 500ms intermixed with content_length matched by labels
+// duration > 500ms and content_length
+//
+// - Requests bigger than 200 KB
+// content_length > 200
+// content_length > 200 and duration
+//
+// - RPS per series
+// rate(integral(duration > bool 0)[1s])
+//
+// - RPS total
+// sum(rate(integral(duration > bool 0)[1s]))
+//
+// - RPS by HTTP method
+// sum(rate(integral(duration > bool 0)[1s])) on "method"
+//
+// - Throughput (MB/s) as a moving 5m window
+// rate(integral(content_length / (1024 * 1024))[5m])
+//
+// - Request duration distribution
+// TODO: ...
+
 impl Engine {
     pub fn new() -> Self {
         Self {}
