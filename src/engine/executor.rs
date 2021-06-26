d03fa6e60b5a218cae02c39c1c80040c886f40a4
Ivan Velichko <iximiuz@gmail.com>
2021-06-26 14:42:18 +0200

    remove stale comment

--- src/engine/executor.rs
+++ src/engine/executor.rs
@@ -15,68 +15,6 @@ use crate::input::Input;
 use crate::output::Output;
 use crate::parser::ast::*;
 
-// Simple use cases (filtration)
-//
-//     - Requests longer than 500ms
-//     duration > 500ms
-//
-//     - Requests longer than 500ms intermixed with content_length matched by labels
-//     duration > 500ms and content_length (but that's an advanced case)
-//
-//     - Requests bigger than 200 KB
-//     content_length > 200
-//     content_length > 200 and duration (but that's an advanced case)
-//
-//
-// Advanced use cases (with resampling)
-//
-//     - RPS per series
-//     rate(integral(duration > bool 0)[1s])
-//
-//     - RPS total
-//     sum(rate(integral(duration > bool 0)[1s]))
-//
-//     - RPS by HTTP method
-//     sum(rate(integral(duration > bool 0)[1s])) on "method"
-//
-//     - Throughput (MB/s) as a moving 5m window
-//     rate(integral(content_length / (1024 * 1024))[5m])
-//
-//     - Request duration distribution
-//     TODO: ...
-//
-// Advanced use cases require defining an evaluation step. I.e. every rate() calculation
-// should be reported at some constant frequency (unlike the original samples that may
-// appear at random times). Every aggregation such as sum() takes all the series (vertical
-// axis) at a give sampling step and combines them. That's how different series are aligned
-// in time. And since we define the time alignment, we can start combining instant vectors
-// using the original Prometheus rules - by matching labels.
-
-// Time axis is the horizontal one.
-// Series axis is the vertical one.
-//
-// Range-vectors essentialy defines a moving time window.
-//
-// Horizontal functions accept range-vectors and do the aggregation over the time axis:
-//   - rate
-//   - increase
-//   - delta
-//   - <agg>_over_time
-//   - ...
-//
-// Vertical functions accept instant-vectors and do modification of its values:
-//   - abs
-//   - ceil
-//   - exp
-//   - log
-//   - ...
-//
-// [some] Operators accept instant-vectos and do the aggregation over the series axis:
-//   - sum [on] - group time series
-//   - min/max/avg/topk/bottomk
-//   - count
-//   - ...
-
 const DEFAULT_INTERVAL: Duration = Duration::from_millis(1000);
 const DEFAULT_LOOKBACK: Duration = DEFAULT_INTERVAL;
 
