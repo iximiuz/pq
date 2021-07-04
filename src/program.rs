04fb2b093dd314e9eb3f90bcf535be7115d0556a
Ivan Velichko <iximiuz@gmail.com>
2021-07-04 18:21:44 +0200

    mapper parser!

--- src/program.rs
+++ src/program.rs
@@ -1,10 +1,13 @@
 use crate::error::{Error, Result};
-use crate::utils::parse::{maybe_lpadded, IResult, ParseError, Span};
+use crate::utils::parse::{
+    label_identifier, maybe_lpadded, separated_list, string_literal, IResult, ParseError, Span,
+};
 
 use nom::{
     branch::alt,
     bytes::complete::{tag_no_case, take},
-    character::complete::char,
+    character::complete::{char, digit1},
+    combinator::{map, value},
     sequence::preceded,
 };
 
@@ -33,7 +36,31 @@ pub enum Decoder {
 }
 
 #[derive(Debug)]
-pub struct Mapper {}
+pub struct Mapper {
+    pub fields: Vec<MapperField>,
+}
+
+#[derive(Debug)]
+pub struct MapperField {
+    loc: FieldLoc,
+    typ: FieldType,
+    alias: Option<String>,
+}
+
+#[derive(Debug)]
+pub enum FieldLoc {
+    Position(usize),
+    Name(String),
+}
+
+#[derive(Clone, Debug)]
+pub enum FieldType {
+    Auto,
+    Number,
+    String,
+    Const(String),
+    Timestamp(Option<String>),
+}
 
 #[derive(Debug)]
 pub enum Formatter {
@@ -133,8 +160,159 @@ fn decoder(input: Span) -> IResult<Decoder> {
 }
 
 fn mapper(input: Span) -> IResult<Mapper> {
-    let (rest, _) = tag_no_case("implement me!")(input)?;
-    Ok((rest, Mapper {}))
+    let (rest, _) = tag_no_case("map")(input)?;
+    let (rest, fields) = match maybe_lpadded(separated_list(
+        '{',
+        '}',
+        ',',
+        mapper_field,
+        "map expression",
+        "field definition like '.foo:str' or '}'",
+    ))(rest)
+    {
+        Ok((rest, fields)) => (rest, fields),
+        Err(nom::Err::Error(_)) => {
+            return Err(nom::Err::Failure(ParseError::partial(
+                "map expression",
+                "comma-separated list of fields",
+                rest,
+            )))
+        }
+        Err(e) => return Err(e),
+    };
+    Ok((rest, Mapper { fields }))
+}
+
+fn mapper_field(input: Span) -> IResult<MapperField> {
+    alt((mapper_field_dynamic, mapper_field_const))(input)
+}
+
+// .0:ts "%Y-%m-%d" as time
+// .1 as method
+// .foo:num
+// .qux:str as bar,
+fn mapper_field_dynamic(input: Span) -> IResult<MapperField> {
+    let (rest, _) = char('.')(input)?;
+
+    let (rest, loc) = match alt((
+        map(digit1, |d: Span| {
+            FieldLoc::Position((*d).parse::<usize>().unwrap())
+        }),
+        map(label_identifier, |n| FieldLoc::Name(n)),
+    ))(rest)
+    {
+        Ok((rest, loc)) => (rest, loc),
+        Err(nom::Err::Error(_)) => {
+            return Err(nom::Err::Failure(ParseError::partial(
+                "map expression",
+                "field position (number) or name (identifier)",
+                rest,
+            )));
+        }
+        Err(e) => return Err(e),
+    };
+
+    let (rest, typ) = match maybe_lpadded(mapper_field_type)(rest) {
+        Ok((rest, typ)) => (rest, typ),
+        Err(nom::Err::Error(_)) => (rest, FieldType::Auto),
+        Err(e) => return Err(e),
+    };
+
+    let (rest, alias) = match maybe_lpadded(mapper_field_alias)(rest) {
+        Ok((rest, alias)) => (rest, Some(alias)),
+        Err(nom::Err::Error(_)) => (rest, None),
+        Err(e) => return Err(e),
+    };
+
+    Ok((rest, MapperField { loc, typ, alias }))
+}
+
+fn mapper_field_type(input: Span) -> IResult<FieldType> {
+    let (rest, _) = char(':')(input)?;
+
+    let (rest, typ) = match maybe_lpadded(alt((
+        value(FieldType::String, maybe_lpadded(tag_no_case("str"))),
+        value(FieldType::Number, maybe_lpadded(tag_no_case("num"))),
+        value(FieldType::Timestamp(None), maybe_lpadded(tag_no_case("ts"))),
+    )))(rest)
+    {
+        Ok((rest, typ)) => (rest, typ),
+        Err(nom::Err::Error(_)) => {
+            return Err(nom::Err::Failure(ParseError::partial(
+                "map expression",
+                "type (str, num, or ts)",
+                rest,
+            )));
+        }
+        Err(e) => return Err(e),
+    };
+
+    if let FieldType::Timestamp(_) = typ {
+        let (rest, format) = match maybe_lpadded(string_literal)(rest) {
+            Ok((rest, format)) => (rest, Some(format)),
+            Err(nom::Err::Error(_)) => (rest, None),
+            Err(e) => return Err(e),
+        };
+        return Ok((rest, FieldType::Timestamp(format)));
+    }
+
+    Ok((rest, typ))
+}
+
+fn mapper_field_alias(input: Span) -> IResult<String> {
+    let (rest, _) = tag_no_case("as ")(input)?;
+
+    let (rest, alias) = match maybe_lpadded(label_identifier)(rest) {
+        Ok((rest, alias)) => (rest, alias),
+        Err(nom::Err::Error(_)) => {
+            return Err(nom::Err::Failure(ParseError::partial(
+                "map expression",
+                "alias (identifier)",
+                rest,
+            )));
+        }
+        Err(e) => return Err(e),
+    };
+
+    Ok((rest, alias))
+}
+
+// extra_label: "value"
+fn mapper_field_const(input: Span) -> IResult<MapperField> {
+    let (rest, name) = label_identifier(input)?;
+
+    let rest = match maybe_lpadded(char(':'))(rest) {
+        Ok((rest, _)) => rest,
+        Err(nom::Err::Error(_)) => {
+            return Err(nom::Err::Failure(ParseError::partial(
+                "map expression",
+                ":",
+                rest,
+            )));
+        }
+        Err(e) => return Err(e),
+    };
+
+    let (rest, value) = match maybe_lpadded(string_literal)(rest) {
+        Ok((rest, value)) => (rest, value),
+        Err(nom::Err::Error(_)) => {
+            return Err(nom::Err::Failure(ParseError::partial(
+                "map expression",
+                "string literal",
+                rest,
+            )));
+        }
+        Err(e) => return Err(e),
+    };
+
+    Ok((
+        rest,
+        MapperField {
+            loc: FieldLoc::Name(name),
+            typ: FieldType::Const(value.to_owned()),
+            alias: None,
+        },
+    ))
 }
 
 fn query(input: Span) -> IResult<String> {
@@ -188,6 +366,9 @@ mod tests {
             r#"json |to_json"#,
             r#"json|to_json"#,
             r#"/.*(\\d+)foo\\s(\\w+).+/ | to_json"#,
+            r#"/.*(\\d+)foo\\s(\\w+).+/ | map {foo: "bar"} | to_json"#,
+            r#"/.*(\\d+)foo\\s(\\w+).+/ | map {.0:str, .1:num as qux, .2:ts "%Y-%m-%d", foo: "bar"} | to_json"#,
+            r#"/.*(\\d+)foo\\s(\\w+).+/ | map {.foo:str as bar, .qux:num, .ts:ts "%Y-%m-%d", abc: "42"} | to_json"#,
         ];
 
         for input in &tests {
